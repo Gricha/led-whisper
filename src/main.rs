818604ef54482342cd0a4a0acd76f7e9b396835c
Greg Pstrucha <gricha@localhost>
2024-01-01 23:15:25 +0000

    Set up actual recording to a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, post},
+    Router,
+};
+mod audio;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/_status", get(status))
+        .route("/record", post(record));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn status() -> &'static str {
+    "OK"
+}
+
+async fn record() -> &'static str {
+    // ideally triggers recording and returns ack early
+
+    audio::start_recording().await.expect("success?");
+
+    "Started recording"
 }
